ae531d4f5391f05f52de62a189a8f9b195c41fc9
bowpie <bogdanungureanu006@gmail.com>
2022-07-02 16:01:10 +0300

    some changes

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ from asyncio.windows_events import SelectorEventLoop
 from discord.ext import commands
 from serverclass import MinecraftServer
 
-# Copyright(C) 2021 Bogdan Ungureanu
-# This script makes playing minecraft with your friends easier.
-# You can command the minecraft server from the discord server, but only the allowed commands you configured in config.json
-# The names in allowed-names must be lowercase in order to work
-# The roles in allowed-roles are case-sensitive
+"""
+MIT License
+
+Copyright (c) 2022 Bogdan Ungureanu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+This script makes playing minecraft with your friends easier.
+"""
 
 
 with open("config.json", "r") as f:
